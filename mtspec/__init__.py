a1d62278f5312d1e787f2627bb4a1dc7140bd662
lpenasa <luca.penasa@gmail.com>
2014-03-03 18:38:09 +0100

    first commit

---
+++ mtspec/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+A Python wrapper for the multitaper library of German A. Prieto (see link_).
+
+:copyright: Moritz Beyreuther and Lion Krischer
+:license: GNU General Public License (GPLv2)
+
+.. _link: http://wwwprof.uniandes.edu.co/~gprieto/software/mwlib.html.
+"""
+
+from mtspec.multitaper import mtspec, sine_psd, dpss, wigner_ville_spectrum, mt_coherence
